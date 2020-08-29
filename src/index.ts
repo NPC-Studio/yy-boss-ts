38dbdb884a9e6cd463c897b60cb8b85196b563d8
Jonathan Spira <jjspira@gmail.com>
2020-08-29 13:51:04 -0700

    working the startup problem still

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,10 @@ import 'child_process';
 import { spawn } from 'child_process';
 import { YY_BOSS_PATH } from './boss_path';
 import { Output, OutputType, Input } from './yy_boss_typings';
-import { YyBoss } from './yy_boss';
+import { YyBoss, createYypBoss } from './yy_boss';
 
 async function main() {
-    const yy_boss_handle = new YyBoss('examples/test_proj/test_proj.yyp', 'examples/wd');
-    let output = await yy_boss_handle.writeCommand({ okay: 3 });
+    const yy_boss_handle = await createYypBoss('examples/test_proj/test_proj.yyp', 'examples/wd');
 }
 
 main();
--- src/yy_boss.ts
+++ src/yy_boss.ts
@@ -1,19 +1,37 @@
 import { spawn, ChildProcessWithoutNullStreams } from 'child_process';
 import { YY_BOSS_PATH } from './boss_path';
-import { Output, Input } from './yy_boss_typings';
+import { Output, Input, OutputType } from './yy_boss_typings';
+import { assert } from 'console';
+
+export function createYypBoss(yypPath: string, wd: string): Promise<[Output, YyBoss | undefined]> {
+    let yyBossHandle = spawn(YY_BOSS_PATH, [yypPath, wd]);
+
+    return new Promise((resolve, _) => {
+        yyBossHandle.stdout.once('data', (chonk: string) => {
+            // if we boof the command somehow, JSON.parse will throw
+            var output: Output = JSON.parse(chonk);
+            assert(output.type === OutputType.Startup);
+
+            // output
+            if (output.success) {
+                resolve(new YyBoss(yyBossHandle));
+            } else {
+                reject(chonk);
+            }
+        });
+    });
+}
 
 export class YyBoss {
     private yyBossHandle: ChildProcessWithoutNullStreams;
 
-    constructor(yypPath: string, wd: string) {
-        this.yyBossHandle = spawn(YY_BOSS_PATH, [yypPath, wd]);
+    constructor(yyBossHandle: ChildProcessWithoutNullStreams) {
+        this.yyBossHandle = yyBossHandle;
     }
 
-    writeCommand(
-        command: Input,
-    ): Promise<Output> {
+    writeCommand(command: Input): Promise<Output> {
         return new Promise((resolve, _) => {
-            this.yyBossHandle.stdout.once("data", chonk => {
+            this.yyBossHandle.stdout.once('data', chonk => {
                 var output: Output = JSON.parse(chonk);
                 resolve(output);
             });
